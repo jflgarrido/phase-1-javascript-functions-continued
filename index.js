5bb07483e6f16c4049f47a31c74050b4aa0399df
Julius Garrido <jflgarrido@gmail.com>
2021-11-02 19:40:54 -0600

    commit tree 84f0033ba58a9b697184da2a64285b2960fe6adf

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+function saturdayFun(x) {
+    return x ? "This Saturday, I want to " + x + "!": 'This Saturday, I want to roller-skate!';    
+}
+
+const mondayWork = y => {
+    return y ? "This Monday, I will " + y + ".": "This Monday, I will go to the office."
+}
+function wrapAdjective(flair = '*') {
+    return function(word = 'special') {
+        return 'You are ' + flair + word + flair + '!';
+    }
+}
